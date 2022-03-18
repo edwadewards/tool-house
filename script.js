dc6cc97347ec2da1cc578ec60f66ae45b49dd155
Wade Edwards <wadecedwards@gmail.com>
2022-03-18 13:20:38 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.documentElement.style.setProperty('overflow', 'auto');
 
+const initialHeight = window.innerHeight;
+
 const metaViewport = document.querySelector('meta[name=viewport]');
 metaViewport.setAttribute('content', 'height=' + initialHeight + 'px, width=device-width, initial-scale=1.0');
